8fa77df330b91066644a7861bc2d4cf89530ac00
cvs2snv <cvs2svn@cs.ru.nl>
2001-04-11 16:13:19 +0000

    This commit was manufactured by cvs2svn to create tag 'stable2'.

--- backendC/CleanCompilerSources/comparser.h
+++
@@ -1,4 +0,0 @@
-extern DefMod ParseDefMod (char *name, char *importingModule, char *importingExtension, unsigned importingLine);
-extern ImpMod ParseImplMod (char *name);
-
-extern void InitParser (void);
