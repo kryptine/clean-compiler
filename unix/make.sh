ee57dbb657b3782fffa002c4aa58fa8be395d782
Ronny Wichers Schreur <ronny@cs.ru.nl>
2002-06-11 12:24:54 +0000

    Build ArgEnvC.o before linking

--- unix/make.sh
+++ unix/make.sh
@@ -2,6 +2,7 @@
 CLM=clm
 
 (cd backendC/CleanCompilerSources; make)
+(cd ArgEnvUnix; make ArgEnvC.o)
 $CLM -h 20M -nt -nw -ci -ns -I ArgEnvUnix -I backend -I compiler -I main -I main/Unix -I WrapDebug \
 	-l ArgEnvUnix/ArgEnvC.o \
 	-l main/Unix/cDirectory.o \
