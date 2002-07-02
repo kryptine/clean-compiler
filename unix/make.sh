de860577886e5c0df3d69e83f8e4e92608caf0c9
Ronny Wichers Schreur <ronny@cs.ru.nl>
2002-07-02 17:54:08 +0000

    Make all object files in the Unix directory.

--- unix/make.sh
+++ unix/make.sh
@@ -3,7 +3,7 @@ CLM=clm
 
 (cd backendC/CleanCompilerSources; make)
 (cd ArgEnvUnix; make ArgEnvC.o)
-(cd main/Unix; make ipc.o; make set_return_code_c.o)
+(cd main/Unix; make all);
 $CLM -h 20M -nt -nw -ci -ns -nr -I ArgEnvUnix -I backend -I compiler -I main -I main/Unix -I WrapDebug \
 	-l ArgEnvUnix/ArgEnvC.o \
 	-l main/Unix/cDirectory.o \
