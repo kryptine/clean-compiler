903617cfba5c1a667d19341821b6b5bb2d169a0b
Ronny Wichers Schreur <ronny@cs.ru.nl>
2002-07-03 09:23:49 +0000

    Reduced default compiler heap size from 20M to 16M.

--- unix/make.solaris.sh
+++ unix/make.solaris.sh
@@ -4,7 +4,7 @@ CLM=clm
 (cd backendC/CleanCompilerSources; make)
 (cd ArgEnvUnix; make ArgEnvC.o)
 (cd main/Unix; make -f Makefile.solaris all);
-$CLM -h 20M -nt -nw -ci -ns -nr -I ArgEnvUnix -I backend -I compiler -I main -I main/Unix -I WrapDebug \
+$CLM -h 16M -nt -nw -ci -ns -nr -I ArgEnvUnix -I backend -I compiler -I main -I main/Unix -I WrapDebug \
 	-l ArgEnvUnix/ArgEnvC.o \
 	-l main/Unix/cDirectory.o \
 	-l main/Unix/set_return_code_c.o \
