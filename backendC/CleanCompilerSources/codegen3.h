76a21b3cebccd4ac914a60903f6ae53b3584717a
John van Groningen <johnvg@cs.ru.nl>
2002-10-21 13:08:53 +0000

    add missing prototypes

--- backendC/CleanCompilerSources/codegen3.h
+++ backendC/CleanCompilerSources/codegen3.h
@@ -11,3 +11,11 @@ struct saved_node_id_ref_counts* save_lhs_node_id_ref_counts (NodeP node_p,struc
 struct saved_node_id_ref_counts* save_rhs_node_id_ref_counts (NodeP node_p,NodeDefP node_defs,
 											struct saved_node_id_ref_counts *snir_p,struct saved_case_node_id_ref_counts ***scnirc_h);
 void restore_node_id_ref_counts (struct saved_node_id_ref_counts *snir_p,struct saved_case_node_id_ref_counts *scnirc_p);
+
+#if TAIL_CALL_MODULO_CONS_OPTIMIZATION
+int does_tail_call_modulo_cons (NodeP node_p,NodeDefP node_defs);
+#endif
+
+#if OPTIMIZE_LAZY_TUPLE_RECURSION
+void update_tuple_element_node (StateP state_p,int tuple_element_a_index,int *asp_p,int *bsp_p);
+#endif
