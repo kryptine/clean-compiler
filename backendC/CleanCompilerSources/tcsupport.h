ac4c64d2adaab82612101fae92e2dc2deb293600
John van Groningen <johnvg@cs.ru.nl>
2004-02-20 16:46:12 +0000

    remove prototypes of removed functions, move some prototypes from typechecker or overloading header files to this file

--- backendC/CleanCompilerSources/tcsupport.h
+++ backendC/CleanCompilerSources/tcsupport.h
@@ -6,33 +6,8 @@ Author: Sjaak Smetsers
 
 */
 
-extern jmp_buf ExitTypeComponent;
+extern BITVECT DetermineUniPropOfTypeCons (SymbDef typecons);
 
-extern void OverloadingError (Symbol symbol, char *msg, TypeCell type, Bool make_jump);
+extern BITVECT DetermineConsVarsOfTypeCons (SymbDef typecons, ConsVarList * cons_vars);
 
-extern void Unify (TypeCell offtype, TypeCell demtype, Node uni_node, int argnr);
-
-extern UnificationStatus UnifyTypes (TypeCell offtype, TypeCell demtype);
-
-extern void UnifyError (UnificationStatus ustat, Node err_node, int err_argnr, TypeCell type1, TypeCell type2);
-
-extern void UniquenessError (UniquenessErrorKind err_kind, Node err_node, int err_argnr, TypeCell type, TypeCell sub_type);
-
-extern void ReportTypeError (Node err_node, int err_argnr, char *err_msg);
-
-extern TypeCell ExpandSynonymType (TypeCell synappl, SymbDef syndef);
-
-extern AttributeCellKind DetermineAttrkindOfTypeCell (TypeCell type);
-
-#define GetExistentionalVarsOfTypeCons(typecons) (typecons -> sdef_contains_freevars) ?\
-		typecons -> sdef_type -> type_exivars : ALLBITSCLEAR
-
-extern Symbol BuildNewSymbol (SymbDef old_symb_def, int id_nr, TypeCell ins_types [], int arity, TableKind table);
-
-extern Symbol BuildNewClassSymbol (SymbolList class_symbols);
-
-extern Ident BuildNewSymbolId (char *prefix, int id_nr, TypeCell ins_types [], int arity, TableKind table);
-
-extern TypeCell SkipTypeSynIndirection (TypeCell type);
-
-extern void PrepareTypesAndImportedInstances (Symbol symbs, char *icl_module);
+extern void PrintNodeSymbol (Node node, int arg_nr, File file);
