fde739ab23b1c77d2a41784d1f6d9f42f7551999
John van Groningen <johnvg@cs.ru.nl>
2011-02-01 13:46:11 +0000

    remove building ArgEnv

--- unix/make.macosx64.sh
+++ unix/make.macosx64.sh
@@ -2,7 +2,6 @@
 CLM=clm
 
 (cd backendC/CleanCompilerSources; make -f Makefile.linux64)
-(cd ../libraries/ArgEnvUnix; make ArgEnvC.o)
 (cd main/Unix; make -f Makefile all);
 $CLM -gcm -h 40M -nt -nw -ci -nr -IL ArgEnv -I backend -I frontend -I main -I main/Unix \
 	-l backendC/CleanCompilerSources/backend.a \
