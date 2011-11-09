66a177f9925620e98111a6e5dfa07b7e16ecb0d2
John van Groningen <johnvg@cs.ru.nl>
2011-11-09 11:30:59 +0000

    add make.macosx64.sh

---
+++ unix/make.macosx64.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+CLM=clm
+
+(cd backendC/CleanCompilerSources; make -f Makefile.linux64)
+(cd main/Unix; make -f Makefile all);
+$CLM -gcm -h 40M -nt -nw -ci -nr -IL ArgEnv -I backend -I frontend -I main -I main/Unix \
+	-l backendC/CleanCompilerSources/backend.a \
+	cocl -o cocl
