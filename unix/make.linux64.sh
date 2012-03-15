d974de8cc155dc85243619d8c24625430418b9fb
John van Groningen <johnvg@cs.ru.nl>
2012-03-15 11:34:50 +0000

    use standard ArgEnv, increase heap size

--- unix/make.linux64.sh
+++ unix/make.linux64.sh
@@ -2,11 +2,9 @@
 CLM=clm
 
 (cd backendC/CleanCompilerSources; make -f Makefile.linux64)
-(cd ../libraries/ArgEnvUnix; make ArgEnvC.o)
 (cd main/Unix; make -f Makefile all);
-$CLM -gcm -h 40M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
-	-I ../libraries/ArgEnvUnix \
-	-l ../libraries/ArgEnvUnix/ArgEnvC.o \
+$CLM -gcm -h 80M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
+	-IL ArgEnv \
 	-l main/Unix/Clean\ System\ Files/cDirectory.o \
 	-l main/Unix/Clean\ System\ Files/set_return_code_c.o \
 	-l main/Unix/Clean\ System\ Files/ipc_c.o \
