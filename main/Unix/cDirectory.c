e86bfa0cb82d21aed4fbc7fd8b09077cdaf401a8
John van Groningen <johnvg@cs.ru.nl>
2013-10-24 14:03:37 +0000

    fix bug in function getCommonFileInfoC on 64 bit platforms

--- main/Unix/cDirectory.c
+++ main/Unix/cDirectory.c
@@ -103,7 +103,7 @@ void getCommonFileInfoC(int also_get_file_name,
 {
     struct tm   *pModificationTime;
     int         mask;
-    static int null = 0;
+    static void *null = NULL; /* int 0 with the size of a Clean INT */
 
     *pFileName      = also_get_file_name ? (CleanString) gFileName : (CleanString) &null;
     *pFileSizeLow   = gFileStat.st_size;
