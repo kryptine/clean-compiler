9e5e214e12d47f4dbb7a65abfa1029a1c2990aa7
Jurrien Stutterheim <jstutterheim@cs.ru.nl>
2015-11-17 11:09:52 +0000

    More include paths for itasks/unix/make.macosx64.sh

--- unix/make.macosx64.sh
+++ unix/make.macosx64.sh
@@ -3,6 +3,6 @@ CLM=clm
 
 (cd backendC/CleanCompilerSources; make -f Makefile.linux64)
 (cd main/Unix; make -f Makefile all);
-$CLM -gcm -h 80M -s 4m -nt -nw -ci -nr -IL ArgEnv -I backend -I frontend -I main -I main/Unix \
+$CLM -gcm -h 256M -s 8m -nt -nw -ci -nr -IL ArgEnv -IL StdLib -IL Dynamics -I backend -I frontend -I main -I main/Unix \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
