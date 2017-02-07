e7380031e0cdfe0681d1364f45ad70d71f58f915
John van Groningen <johnvg@cs.ru.nl>
2017-02-07 16:08:05 +0000

    add make.linux_arm.sh that uses ARM Makefiles

---
+++ unix/make.linux_arm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+CLM=clm
+
+(cd backendC/CleanCompilerSources; make -f Makefile.arm)
+(cd ../libraries/ArgEnvUnix; make -f Makefile_arm ArgEnvC.o)
+(cd main/Unix; make -f Makefile.arm all);
+$CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
+$CLM -h 24M -nt -nw -ci -ns -nr -I backend -I frontend -I main -I main/Unix \
+	-I ../libraries/ArgEnvUnix \
+	-l backendC/CleanCompilerSources/backend.a \
+	cocl -o cocl
