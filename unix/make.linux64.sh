8c59816a44685925a5e3e99b45f6b2720795c038
John van Groningen <johnvg@cs.ru.nl>
2017-07-18 12:31:12 +0000

    don't include object files generated by the C compiler twice

--- unix/make.linux64.sh
+++ unix/make.linux64.sh
@@ -6,9 +6,5 @@ CLM=clm
 (cd main/Unix; make -f Makefile all);
 $CLM -gcm -h 60M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-I ../libraries/ArgEnvUnix \
-	-l ../libraries/ArgEnvUnix/ArgEnvC.o \
-	-l main/Unix/Clean\ System\ Files/cDirectory.o \
-	-l main/Unix/Clean\ System\ Files/set_return_code_c.o \
-	-l main/Unix/Clean\ System\ Files/ipc_c.o \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
