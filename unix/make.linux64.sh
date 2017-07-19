808497e11e669b84f095427528973cd5b9aa0208
John van Groningen <johnvg@cs.ru.nl>
2017-07-19 11:57:05 +0000

    increase stack size of the compiler

--- unix/make.linux64.sh
+++ unix/make.linux64.sh
@@ -4,7 +4,7 @@ CLM=clm
 (cd backendC/CleanCompilerSources; make -f Makefile.linux64)
 (cd ../libraries/ArgEnvUnix; make ArgEnvC.o)
 (cd main/Unix; make -f Makefile all);
-$CLM -gcm -h 60M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
+$CLM -gcm -h 60M -s 2M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-I ../libraries/ArgEnvUnix \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
