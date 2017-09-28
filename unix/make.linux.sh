939ad9ef04cc93d0cce09078e8039a6332c4daf7
John van Groningen <johnvg@cs.ru.nl>
2017-09-28 10:30:35 +0000

    strip the compiler executables, increase default heap size of the compiler on 32 bit linux to 32M

--- unix/make.linux.sh
+++ unix/make.linux.sh
@@ -5,11 +5,7 @@ CLM=clm
 (cd ../libraries/ArgEnvUnix; make ArgEnvC.o)
 (cd main/Unix; make -f Makefile all);
 $CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
-$CLM -h 24M -nt -nw -ci -ns -nr -I backend -I frontend -I main -I main/Unix \
+$CLM -h 32M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-I ../libraries/ArgEnvUnix \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
-#	-l ../libraries/ArgEnvUnix/ArgEnvC.o \
-#	-l main/Unix/cDirectory.o \
-#	-l main/Unix/set_return_code_c.o \
-#	-l main/Unix/ipc.o \
--- unix/make.linux_arm.sh
+++ unix/make.linux_arm.sh
@@ -5,7 +5,7 @@ CLM=clm
 (cd ../libraries/ArgEnvUnix; make -f Makefile_arm ArgEnvC.o)
 (cd main/Unix; make -f Makefile.arm all);
 $CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
-$CLM -h 24M -nt -nw -ci -ns -nr -I backend -I frontend -I main -I main/Unix \
+$CLM -h 32M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-I ../libraries/ArgEnvUnix \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
