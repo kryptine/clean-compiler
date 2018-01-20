5699affe8a6dd40807d4ae6d88761e1fb7748bea
Bas Lijnse <b.lijnse@cs.ru.nl>
2018-01-20 06:43:57 +0000

    Increased default cocl stack size to 16m for windows, linux64 and mac

--- unix/make.linux64.sh
+++ unix/make.linux64.sh
@@ -3,7 +3,7 @@ CLM=clm
 
 (cd backendC/CleanCompilerSources; make -f Makefile.linux64)
 (cd main/Unix; make -f Makefile all);
-$CLM -gcm -h 80M -s 4m -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
+$CLM -gcm -h 80M -s 16m -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-IL ArgEnv \
 	-l main/Unix/Clean\ System\ Files/cDirectory.o \
 	-l main/Unix/Clean\ System\ Files/set_return_code_c.o \
