8dcc7e379eedb735d8227a9d967973971a29bfd4
johnvg@science.ru.nl <johnvg@science.ru.nl>
2019-09-24 15:26:28 +0200

    compiler module backendconvert with -fusion in make.linux.sh, remove cDirectory.o, set_return_code_c.o and ipc_c.o arguments in call to clm

--- unix/make.linux.sh
+++ unix/make.linux.sh
@@ -4,10 +4,8 @@ CLM=clm
 (cd backendC/CleanCompilerSources; make)
 (cd ../libraries/ArgEnvUnix; make ArgEnvC.o)
 (cd main/Unix; make -f Makefile all);
-$CLM -h 40M -nt -nw -ci -ns -nr -I backend -I frontend -I main -I main/Unix \
+$CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
+$CLM -h 40M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-IL ArgEnv \
-	-l main/Unix/cDirectory.o \
-	-l main/Unix/set_return_code_c.o \
-	-l main/Unix/ipc.o \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
--- unix/make.linux64.sh
+++ unix/make.linux64.sh
@@ -6,8 +6,5 @@ CLM=clm
 $CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
 $CLM -gcm -h 80M -s 16m -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-IL ArgEnv \
-	-l main/Unix/Clean\ System\ Files/cDirectory.o \
-	-l main/Unix/Clean\ System\ Files/set_return_code_c.o \
-	-l main/Unix/Clean\ System\ Files/ipc_c.o \
 	-l backendC/CleanCompilerSources/backend.a \
 	cocl -o cocl
