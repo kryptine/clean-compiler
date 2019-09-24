1b3cf5d543dc66ceaa52868d6cf823c1d65fe2ed
johnvg@science.ru.nl <johnvg@science.ru.nl>
2019-09-24 15:17:19 +0200

    compile module backendconvert with -fusion in make.linux64.sh

--- unix/make.linux64.sh
+++ unix/make.linux64.sh
@@ -4,6 +4,7 @@ CLM=clm
 (cd backendC/CleanCompilerSources; make -f Makefile.linux64)
 (cd ../libraries/ArgEnvUnix; make ArgEnvC.o)
 (cd main/Unix; make -f Makefile all);
+$CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
 $CLM -gcm -h 60M -s 2M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-I ../libraries/ArgEnvUnix \
 	-l backendC/CleanCompilerSources/backend.a \
