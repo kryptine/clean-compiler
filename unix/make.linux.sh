9081b663a2083820869043976157502d9a34ca6d
Camil Staps <info@camilstaps.nl>
2020-04-08 10:14:45 +0200

    Only build with -m32 on 32-bit unix

--- unix/make.linux.sh
+++ unix/make.linux.sh
@@ -3,7 +3,7 @@ CLM=clm
 
 (cd backendC/CleanCompilerSources; make)
 (cd ../libraries/ArgEnvUnix; make ArgEnvC.o)
-(cd main/Unix; make -f Makefile all);
+(cd main/Unix; CFLAGS=-m32 make -f Makefile all);
 $CLM -ci -I backend -I frontend -I main -I main/Unix -ABC -fusion backendconvert
 $CLM -h 32M -nt -nw -ci -nr -I backend -I frontend -I main -I main/Unix \
 	-IL ArgEnv \
